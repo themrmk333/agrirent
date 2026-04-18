4db94a01de952a20d1b0ac1e5f333f10386ba32f
Mahesh Koli <admin@gmail.com>
2026-04-18 22:23:30 +0530

    render fix

--- app.py
+++ app.py
@@ -731,5 +731,5 @@ if __name__ == '__main__':
     except Exception:
         pass
         
-    app.run(host="0.0.0.0", port=5000, debug=True)
-    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)    
